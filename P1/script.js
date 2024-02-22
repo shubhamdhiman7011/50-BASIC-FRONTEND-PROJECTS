503570d8f30e321404e5d84aa178437d2071a369
Shubham Dhiman <Shubhamdhiman7011@gmail.com>
2024-02-22 13:51:26 +0530

    comment add

--- P1/script.js
+++ P1/script.js
@@ -3,7 +3,7 @@ const panels=document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
+        removeActiveClasses() // function call to remove previous node
         panel.classList.add('active')
     })
 })
